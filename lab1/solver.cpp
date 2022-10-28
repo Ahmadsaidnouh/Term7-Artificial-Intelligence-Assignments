e82b7a626eff4e5108e30e06c6ee05fec6bfdafc
Ahmadsaidnouh <es-ahmad.nouh2025@alexu.edu.eg>
2022-10-28 06:14:30 +0200

    updated state data type to integer & added gui step animation

--- lab1/solver.cpp
+++ lab1/solver.cpp
@@ -8,62 +8,74 @@
 
 using namespace std;
 
-string stringifyPath(vector<string> path)
+string stringifyPath(vector<int> path)
 {
     // don't touch this code please ;_;
     int indexZero, oldR, oldC, newR, newC;
     string newZero, direction, currentZero, stringPath = "";
     for (unsigned long long i = 0; i < path.size() - 1; i++)
     {
-        indexZero = path[i].find('0');
-        oldR = indexZero / 3;
-        oldC = indexZero % 3;
+        if ((path[i] / 100000000) == 0)
+        {
+            indexZero = 0;
+            oldR = 0;
+            oldC = 0;
+        }
+        else
+        {
+            indexZero = to_string(path[i]).find('0');
+            oldR = indexZero / 3;
+            oldC = indexZero % 3;
+        }
         currentZero = to_string(indexZero);
-        // currentZero = "slot" + to_string(indexZero);
 
-        indexZero = path[i+1].find('0');
-        newR = indexZero / 3;
-        newC = indexZero % 3;
+        if ((path[i + 1] / 100000000) == 0)
+        {
+            indexZero = 0;
+            newR = 0;
+            newC = 0;
+        }
+        else
+        {
+            indexZero = to_string(path[i + 1]).find('0');
+            newR = indexZero / 3;
+            newC = indexZero % 3;
+        }
         newZero = to_string(indexZero);
-        // newZero = "slot" + to_string(indexZero);
-     
-        if(oldR > newR) 
+
+        if (oldR > newR)
             direction = "down";
-        else if(oldR < newR) 
+        else if (oldR < newR)
             direction = "up";
-        else if(oldC > newC) 
+        else if (oldC > newC)
             direction = "right";
-        else if(oldC < newC) 
+        else if (oldC < newC)
             direction = "left";
 
         stringPath += newZero + "-" + direction + "-" + currentZero + ",";
     }
-    // stringPath += ";";
     return stringPath;
 }
 
-string parseInitialState(int withoutZero, int indexOfZero)
-{
-    // don't touch this code please ;_;
-    string res = to_string(withoutZero);
-    return res.insert(indexOfZero, "0");
-}
-
 // A utility function to count inversions in given array 'arr[]'
-int getInvCount(string state)
+int getInvCount(int state)
 {
     // don't touch this code please ;_;
+    string strState = to_string(state);
+    if ((state / 100000000) == 0)
+        strState.insert(0, "0");
+
     int inv_count = 0;
     for (int i = 0; i < 9 - 1; i++)
         for (int j = i + 1; j < 9; j++)
-            if ((state[j] - 48) && (state[i] - 48) && ((state[i] - 48) > (state[j] - 48)))
+            if ((strState[j] - 48) && (strState[i] - 48) && ((strState[i] - 48) > (strState[j] - 48)))
                 inv_count++;
 
     return inv_count;
 }
 
 // This function returns true if given 8 puzzle is solvable.
-bool isSolvable(string state)
+bool isSolvable(int state)
 {
     // don't touch this code please ;_;
     // Count inversions in given 8 puzzle
@@ -73,28 +85,31 @@ bool isSolvable(string state)
     return (invCount % 2 == 0);
 }
 
-vector<string> findNeighbors(string state)
+vector<int> findNeighbors(int state)
 {
     // don't touch this code please ;_;
-    vector<string> neighbors;
+    vector<int> neighbors;
+    string strState = to_string(state);
 
-    int indexZero = state.find('0');
+    if ((state / 100000000) == 0)
+        strState.insert(0, "0");
+
+    int indexZero = strState.find('0');
     int rowZero = indexZero / 3;
     int columnZero = indexZero % 3;
 
     int rowTmp, colTmp, newIndexZero;
-
-    state.erase(indexZero);
 
     string sTemp = "";
     rowTmp = rowZero - 1;
     colTmp = columnZero;
     if ((rowTmp <= 2) && (rowTmp >= 0) && (colTmp <= 2) && (colTmp >= 0))
     {
-        sTemp = state;
+        sTemp = strState;
         newIndexZero = rowTmp * 3 + colTmp;
-        sTemp.insert(newIndexZero, "0");
-        neighbors.push_back(sTemp);
+        sTemp[indexZero] = sTemp[newIndexZero];
+        sTemp[newIndexZero] = '0';
+        neighbors.push_back(stoi(sTemp));
     }
 
     sTemp = "";
@@ -102,9 +117,11 @@ vector<string> findNeighbors(string state)
     colTmp = columnZero;
     if ((rowTmp <= 2) && (rowTmp >= 0) && (colTmp <= 2) && (colTmp >= 0))
     {
-        sTemp = state;
+        sTemp = strState;
         newIndexZero = rowTmp * 3 + colTmp;
-        sTemp.insert(newIndexZero, "0");
+        sTemp[indexZero] = sTemp[newIndexZero];
+        sTemp[newIndexZero] = '0';
+        neighbors.push_back(stoi(sTemp));
     }
 
     sTemp = "";
@@ -112,9 +129,11 @@ vector<string> findNeighbors(string state)
     colTmp = columnZero - 1;
     if ((rowTmp <= 2) && (rowTmp >= 0) && (colTmp <= 2) && (colTmp >= 0))
     {
-        sTemp = state;
+        sTemp = strState;
         newIndexZero = rowTmp * 3 + colTmp;
-        sTemp.insert(newIndexZero, "0");
+        sTemp[indexZero] = sTemp[newIndexZero];
+        sTemp[newIndexZero] = '0';
+        neighbors.push_back(stoi(sTemp));
     }
 
     sTemp = "";
@@ -122,33 +141,35 @@ vector<string> findNeighbors(string state)
     colTmp = columnZero + 1;
     if ((rowTmp <= 2) && (rowTmp >= 0) && (colTmp <= 2) && (colTmp >= 0))
     {
-        sTemp = state;
+        sTemp = strState;
         newIndexZero = rowTmp * 3 + colTmp;
-        sTemp.insert(newIndexZero, "0");
+        sTemp[indexZero] = sTemp[newIndexZero];
+        sTemp[newIndexZero] = '0';
+        neighbors.push_back(stoi(sTemp));
     }
 
     return neighbors;
 }
 
-vector<string> solveBFS(string initialState)
+vector<int> solveBFS(int initialState)
 {
-    vector<string> finalPath;
+    vector<int> finalPath;
     // code here
 
     return finalPath;
 }
 
-vector<string> solveDFS(string initialState)
+vector<int> solveDFS(int initialState)
 {
-    vector<string> finalPath;
+    vector<int> finalPath;
     // code here
 
     return finalPath;
 }
 
-vector<string> solveAStar(string initialState, int type)
+vector<int> solveAStar(int initialState, int type)
 {
-    vector<string> finalPath;
+    vector<int> finalPath;
     // code here
     // type = 0 ==> manhattan
     // type = 1 ==> euclidean
@@ -159,12 +180,11 @@ vector<string> solveAStar(string initialState, int type)
 extern "C"
 {
     EMSCRIPTEN_KEEPALIVE
-    void solve(int withoutZero, int indexOfZero, int algorithmType)
+    void solve(int initialState, int algorithmType)
     {
         // don't touch this code please ;_;
-        string initialState = parseInitialState(withoutZero, indexOfZero);
         string result = "";
-        vector<string> path;
+        vector<int> path;
         bool isInitialStateSolvable = isSolvable(initialState);
 
         if (isInitialStateSolvable)
@@ -187,14 +207,18 @@ extern "C"
                 path = solveAStar(initialState, 1);
                 break;
             }
-            // The following three lines are for testing only. 
+            // The following three lines are for testing only.
             // Once you have a ready algorithm that return vector of string in path, delete them.
-            path.push_back("125340678");
-            path.push_back("120345678");
-            path.push_back("102345678");
-            path.push_back("012345678");
+            // path.push_back(125340678);
+            // path.push_back(120345678);
+            // path.push_back(102345678);
+            // path.push_back(12345678);
+            path.push_back(125348670);
+            path.push_back(125340678);
+            path.push_back(120345678);
+            path.push_back(102345678);
+            path.push_back(12345678);
 
-            
             result = stringifyPath(path);
         }
         else

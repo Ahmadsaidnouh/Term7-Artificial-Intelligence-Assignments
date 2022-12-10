d877cdb137f19f40fedb42146481286d7eeb48bb
Ahmadsaidnouh <es-ahmad.nouh2025@alexu.edu.eg>
2022-12-11 00:26:00 +0200

    apdated template

--- lab3/template/template.py
+++ lab3/template/template.py
@@ -9,22 +9,22 @@ print(trainingSet.tail(5))  # displays the last 5 elements in training data
 print(testingSet.tail(5))  # displays the last 5 elements in testing data
 
 X_train = trainingSet.iloc[:, :-1]  # X_train contains the features only without the class
-Y_train = trainingSet.iloc[:, -1]  # Y_train contains the class only without the features
+y_train = trainingSet.iloc[:, -1]  # Y_train contains the class only without the features
 X_test = testingSet.iloc[:, :-1]  # X_test contains the features only without the class
-Y_test = testingSet.iloc[:, -1]  # Y_test contains the class only without the features
+y_test = testingSet.iloc[:, -1]  # Y_test contains the class only without the features
 
 print(trainingSet.head(5))  # displays the first 5 elements in training data
 print(X_train.head(5))  # displays the features of first 5 elements in training data
-print(Y_train.head(5))  # displays the class of first 5 elements in training data
+print(y_train.head(5))  # displays the class of first 5 elements in training data
 
 print(testingSet.head(5))  # displays the first 5 elements in testing data
 print(X_test.head(5))  # displays the features of first 5 elements in testing data
-print(Y_test.head(5))  # displays the class of first 5 elements in testing data
+print(y_test.head(5))  # displays the class of first 5 elements in testing data
 
 print(len(X_train))
 print(len(X_test))
-print(len(Y_train))
-print(len(Y_test))
+print(len(y_train))
+print(len(y_test))
 
 # start importing your model from sklearn library
 # then, train your model using X_train and Y_train

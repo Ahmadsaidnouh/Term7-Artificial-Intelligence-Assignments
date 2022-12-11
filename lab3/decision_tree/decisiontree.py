0eca4c85e9667e9ac092623e19feb35511c53a83
ahmed alaa <ahmedtawfiiiq24@gmail.com>
2022-12-11 19:49:37 +0200

    update

---
+++ lab3/decision_tree/decisiontree.py
@@ -0,0 +1,23 @@
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn import tree
+import pandas as pd
+
+trainingSet = pd.read_csv('../finalData/training.csv')
+testingSet = pd.read_csv('../finalData/testing.csv')
+
+# training data
+x_train = trainingSet.iloc[:, :-1]  # features
+y_train = trainingSet.iloc[:, -1]  # labels
+
+# testing data
+x_test = testingSet.iloc[:, :-1]  # features
+y_test = testingSet.iloc[:, -1]  # labels
+
+classifier = tree.DecisionTreeClassifier(criterion='entropy')
+classifier.fit(x_train, y_train)
+
+y_predicted = classifier.predict(x_test)
+
+print(classification_report(y_test, y_predicted))
+print(confusion_matrix(y_test, y_predicted))
